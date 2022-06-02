7fe9be1da8c7566507d7a49ca0ba8d85c3d8a8c2
asajda9496@rcs-k12.us <asajda9496@rcs-k12.us>
2022-06-02 18:39:34 -0400

    Latest push

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -5,11 +5,15 @@
 package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.RelativeEncoder;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import edu.wpi.first.util.sendable.SendableBuilder;
 import edu.wpi.first.wpilibj.AnalogInput;
 import edu.wpi.first.wpilibj.RobotController;
 
@@ -28,14 +32,20 @@ public class SwerveModule extends SubsystemBase {
   private final boolean absoluteEncoderInversed;
 
 
+
+  private final PIDController spinPID;
+
+  private double kPSpin = 0.5;
+
+
   /** Creates a new SwerveModule. */
   public SwerveModule(int driverMotorPort, int spinMotorPort, boolean driveMotorInverse, boolean spinMotorInverse, int absoluteEncoderChannel, double absoluteEncoderOffset, boolean absoluteEncoderInversed) {
 
     driverMotor = new CANSparkMax(driverMotorPort, MotorType.kBrushless);
     spinMotor = new CANSparkMax(spinMotorPort, MotorType.kBrushless);
 
-    //driverMotor.setInverted(driveMotorInverse);
-    //spinMotor.setInverted(spinMotorInverse);
+    driverMotor.setInverted(driveMotorInverse);
+    spinMotor.setInverted(spinMotorInverse);
 
     driverEncoder = driverMotor.getEncoder();
     spinEncoder = spinMotor.getEncoder();
@@ -44,7 +54,15 @@ public class SwerveModule extends SubsystemBase {
     this.absoluteEncoderInversed = absoluteEncoderInversed;
     absoluteEncoder = new AnalogInput(absoluteEncoderChannel);
 
+    spinPID = new PIDController(kPSpin, 0, 0);
+    spinPID.enableContinuousInput(-Math.PI, Math.PI);
+
     //NEED TO ADD CONVERSIONS HERE
+
+    driverEncoder.setPositionConversionFactor(Constants.);
+    driverEncoder.setVelocityConversionFactor(Constants.);
+    spinEncoder.setPositionConversionFactor(Constants.);
+    spinEncoder.setVelocityConversionFactor(Constants.);
 
     resetEncoders();
   }
@@ -84,11 +102,29 @@ public class SwerveModule extends SubsystemBase {
     }
   }
 
+  public void setDesiredState(SwerveModuleState state){
+    if (Math.abs(state.speedMetersPerSecond) < 0.001) {
+      stop();
+    }
+    state = SwerveModuleState.optimize(state, getState().angle);
+    driverMotor.set(state.speedMetersPerSecond / Constants.);
+    spinMotor.set(spinPID.calculate(getSpinPosition(), state.angle.getRadians()));
+  }
+
+  public SwerveModuleState getState() {
+    return new SwerveModuleState(getDriverVelocity(), new Rotation2d(getSpinPosition()));
+  }
+
 
 
   public void stop() {
     driverMotor.set(0);
     spinMotor.set(0);
+  }
+
+  public void initSendable(SendableBuilder builder) {
+
+    builder.addDoubleProperty("Spin P values", () -> this.kPSpin, value -> {kPSpin = value;});
   }
 
   @Override
